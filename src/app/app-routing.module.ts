d30dd38dd4bda2b923434caab023924a4b02bd29
Anna <annalisaterrazas@gmail.com>
2020-09-21 18:29:09 +0000

    only project commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { AddTodosComponent } from './components/add-todos/add-todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', redirectTo:'/home', pathMatch:'full'
+  },
+  {
+    path:'home',component: TodoListComponent
+  },
+  {
+    path:'add',component: AddTodosComponent
+  },
+  {
+    path:'todoId/:id',component: TodoDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
